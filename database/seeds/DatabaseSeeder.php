8a214a9a6e01393509a5cb04673acef094ed15bf
dev3 <y.shpylovyi@ielpe.com>
2018-12-06 17:15:44 +0200

    #seed - fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             FirstUserTableSeeder::class,
             AddFakeCompanies::class,
-            FirstUserTableSeeder::class
+            AddFakeEmployees::class
         ]);
     }
 }
